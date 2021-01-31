f37cd1f9d14abba077c6b99ae6ea071110df8b0f
José Bernardes <joseabernardes@gmail.com>
2021-01-31 18:12:48 +0000

    feat-1: Refactor AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+/*
+ * *************************************************************************
+ *
+ * Copyright: José Bernardes, 2020 - 2021
+ *
+ * *************************************************************************
+ */
+
 import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'personal-app';
+
 }
