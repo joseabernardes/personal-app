e1ba680bfd77d11fb10752327511f42a748facab
José Bernardes <joseabernardes@gmail.com>
2021-02-03 23:33:51 +0000

    feat-1: Configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {
+    RouterModule,
+    Routes,
+} from '@angular/router';
 
-const routes: Routes = [];
+import {UnderConstructionComponent} from './shared/under-construction/under-construction.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: UnderConstructionComponent,
+        pathMatch: 'full',
+    },
+    {
+        path: '**',
+        redirectTo: '',
+    },
+
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
 })
 export class AppRoutingModule {
 }
