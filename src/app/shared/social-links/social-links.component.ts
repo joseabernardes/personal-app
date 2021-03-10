b7ee1e2b0babac845d50cbd99a3dbf662eb0d2c8
José Bernardes <joseabernardes@gmail.com>
2021-03-10 23:40:37 +0000

    feat-2: Create SocialLinksComponent

---
+++ src/app/shared/social-links/social-links.component.ts
@@ -0,0 +1,17 @@
+/*
+ * *************************************************************************
+ *
+ * Copyright: José Bernardes, 2020 - 2021
+ *
+ * *************************************************************************
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-social-links',
+    templateUrl: './social-links.component.html',
+    styleUrls: ['./social-links.component.scss'],
+})
+export class SocialLinksComponent {
+}
