20fa3426bef0c0f517f4a824ef641b646cf90a07
José Bernardes <joseabernardes@gmail.com>
2021-03-10 23:34:17 +0000

    feat-2: Create IconComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,15 +10,17 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {UnderConstructionComponent} from './under-construction/under-construction.component';
+import {IconComponent} from './icon/icon.component';
 
 
 @NgModule({
     declarations: [
         UnderConstructionComponent,
+        IconComponent,
     ],
     imports: [
-        CommonModule
-    ]
+        CommonModule,
+    ],
 })
 export class SharedModule {
 }
