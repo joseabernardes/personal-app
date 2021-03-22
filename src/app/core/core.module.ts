cadebf79f9b4bce396f2963f698dbe05a5c38981
José Bernardes <joseabernardes@gmail.com>
2021-03-22 21:08:37 +0000

    feat-2: Create CoreModule

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,19 @@
+/*
+ * *************************************************************************
+ *
+ * Copyright: José Bernardes, 2020 - 2021
+ *
+ * *************************************************************************
+ */
+
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+    ],
+})
+export class CoreModule {
+}
