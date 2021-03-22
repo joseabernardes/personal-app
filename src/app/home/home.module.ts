a2cbc4d6c7f096fd3811ef0754577f7084f32ca1
José Bernardes <joseabernardes@gmail.com>
2021-03-22 21:13:59 +0000

    feat-2: Create HomeModule

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,22 @@
+/*
+ * *************************************************************************
+ *
+ * Copyright: José Bernardes, 2020 - 2021
+ *
+ * *************************************************************************
+ */
+
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {HomeRoutingModule} from './home-routing.module';
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        HomeRoutingModule,
+    ],
+})
+export class HomeModule {
+}
