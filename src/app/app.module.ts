5d97eee72ebbe6fc9abc06692569aff403d0e054
José Bernardes <joseabernardes@gmail.com>
2021-06-02 08:49:43 +0100

    feat-3: Add animations to home page routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
     ],
     imports: [
+        BrowserAnimationsModule,
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
     ],
     providers: [],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
 })
 export class AppModule {
 }
