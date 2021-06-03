b62e2cd09c611977f85c24cd340610e1334b34a6
José Bernardes <joseabernardes@gmail.com>
2021-06-03 14:52:57 +0100

    feat-3: Rename Main page to Welcome page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -13,7 +13,7 @@ import {HomeRoutingModule} from './home-routing.module';
 import {MenuComponent} from './components/menu/menu.component';
 import {SharedModule} from '../shared/shared.module';
 import {HomeComponent} from './home.component';
-import {MainComponent} from './pages/main/main.component';
+import {WelcomeComponent} from './pages/welcome/welcome.component';
 import {AboutComponent} from './pages/about/about.component';
 
 @NgModule({
@@ -21,7 +21,7 @@ import {AboutComponent} from './pages/about/about.component';
         AboutComponent,
         MenuComponent,
         HomeComponent,
-        MainComponent,
+        WelcomeComponent,
     ],
     imports: [
         CommonModule,
