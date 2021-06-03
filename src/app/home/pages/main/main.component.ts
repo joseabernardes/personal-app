7e358f0058dc39f40750f409a5c6c379538bba89
José Bernardes <joseabernardes@gmail.com>
2021-06-03 14:50:07 +0100

    feat-3: Add random messages to Main Page

--- src/app/home/pages/main/main.component.ts
+++ src/app/home/pages/main/main.component.ts
@@ -6,13 +6,29 @@
  * *************************************************************************
  */
 
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-main',
     templateUrl: './main.component.html',
     styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
+    public message: string;
+
+    private _messages = [
+        'Welcome to my Portfolio.',
+        'Welcome aboard.',
+        'It\'s a pleasure to have you here!',
+        'Hi, it\'s nice to see you here.',
+    ];
+
+    ngOnInit(): void {
+        this.message = this._getRandomMessage();
+    }
+
+    private _getRandomMessage(): string {
+        return this._messages[Math.floor(Math.random() * this._messages.length)];
+    }
 }
