c89712c8f7fa88f0ab9b8358d540f91265259083
José Bernardes <joseabernardes@gmail.com>
2021-06-30 19:49:02 +0100

    feat-9: Add Google Analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+    NgxGoogleAnalyticsModule,
+    NgxGoogleAnalyticsRouterModule,
+} from 'ngx-google-analytics';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -13,6 +17,8 @@ import {AppComponent} from './app.component';
         BrowserAnimationsModule,
         BrowserModule,
         AppRoutingModule,
+        NgxGoogleAnalyticsModule.forRoot('G-J635NMKTWD'),
+        NgxGoogleAnalyticsRouterModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
