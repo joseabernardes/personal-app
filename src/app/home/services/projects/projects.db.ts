e9b337b51a1f267022ce3b54b1ce84180e8c0e97
José Bernardes <joseabernardes@gmail.com>
2021-12-05 12:18:52 +0000

    feat-5: Create Project database file

---
+++ src/app/home/services/projects/projects.db.ts
@@ -0,0 +1,33 @@
+/*
+ * *************************************************************************
+ *
+ * Copyright: José Bernardes, 2020 - 2021
+ *
+ * *************************************************************************
+ */
+
+export enum TechnologyType {
+    Android,
+    Firebase,
+    Maps,
+}
+
+export interface Project {
+    name: string;
+    description: string;
+    mainImage: string;
+    technologies: TechnologyType[];
+}
+
+export const PROJECTS: Project[] = [
+    {
+        name: 'Around Tâmega e Sousa',
+        description: 'A mobile application for managing points of interest in the Tâmega e Sousa Intermunicipal Community.\n',
+        mainImage: '/assets/images/around-tamega-e-sousa.png',
+        technologies: [
+            TechnologyType.Android,
+            TechnologyType.Firebase,
+            TechnologyType.Maps,
+        ],
+    },
+];
